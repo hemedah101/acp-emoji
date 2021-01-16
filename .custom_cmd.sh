5a5fcee26c50eb49a81bc528d375344c6ef9cb4a
Abdallah Hemedah <hemedah94@gmail.com>
2021-01-16 16:39:50 +0200

    :sparkles:feat: update to follow AngularJS Git Commit Message Conventions

--- .custom_cmd.sh
+++ .custom_cmd.sh
@@ -7,27 +7,25 @@ function acp() {
 	if [[ "$1" == "init" ]] then
 		git commit -m ":tada: initial commit" -S
 		
-	elif [[ "$1" == "feature" ]] then
-		git commit -m ":sparkles: $2" -S
-		
-	elif [[ "$1" == "update" ]] then
-		git commit -m ":wrench: $2" -S
-		
+	elif [[ "$1" == "feat" ]] then
+		git commit -m ":sparkles:feat: $2" -S
+
 	elif [[ "$1" == "fix" ]] then
-		git commit -m ":bug: $2" -S
-		
-	elif [[ "$1" == "hotfix" ]] then
-		git commit -m ":ambulance: $2" -S
-		
+		git commit -m ":bug:fix: $2" -S
+				
+	elif [[ "$1" == "docs" ]] then
+		git commit -m ":memo: $2" -S
+
+	elif [[ "$1" == "style" ]] then
+		git commit -m ":pencil2:style: $2" -S
+	
 	elif [[ "$1" == "refactor" ]] then
-		git commit -m ":recycle: $2" -S
+		git commit -m ":recycle:refactor: $2" -S
 		
-	elif [[ "$1" == "release" ]] then
-		git commit -m ":rocket: $2" -S
-	
-	elif [[ "$1" == "package" ]] then
-		git commit -m ":package: $2" -S
+	elif [[ "$1" == "test" ]] then
+		git commit -m ":white_check_mark:test: $2" -S
+				
+	elif [[ "$1" == "chore" ]] then
+		git commit -m ":wrench:chore: $2" -S		
 	fi
-	
-	git push
 }
